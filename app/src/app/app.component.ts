c0708d850d41c8311b293bd8f106bfe98882e43e
wilailuxCsubu <wilailux.ha.57@ubu.ac.th>
2017-10-12 15:47:07 +0700

    Add files via upload

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Healthier_together';
+  Develober = 'Wilailux wanngern' ;
+  ID = '5711403407';
+  
+
+}
